2b241bc932ec0e3389891398b30125c95ff4fa85
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2022-08-10 18:21:15 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+  
+ <h1>
+ Hello this is react app
+ </h1>,
+     
+  document.getElementById('root')
+);
+
+
