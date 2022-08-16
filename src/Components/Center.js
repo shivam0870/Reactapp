766810b5eb6c43f8be97c0e297b2e52a3914e311
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2022-08-17 02:43:43 +0530

    Updated work

--- src/Components/Center.js
+++ src/Components/Center.js
@@ -1,11 +1,78 @@
-import React from 'react';
+// import React from 'react';
+// import {useState} from 'react'
 
-const Center = () => {
-    return(
-    <div>
-        <h3>This is the Center Component</h3>
-    </div>
-    )
-}
+// const Center = () => {
+//     const IncNum = () => {
+        
+//     }
+//     return(
+        
+//     <div>
+//     <h3>This is the Center Component</h3>
 
-export default Center;
+   
+//     <div className='main_cm'>
+  
+   
+        
+//         <h1>0</h1>
+//         <button onClick={IncNum}>Click here </button>
+//     </div>
+//     </div>
+//     )
+// }
+
+// export default Center;
+
+
+
+
+
+
+
+
+import React, { useState } from "react";
+
+const Home = () => {
+   
+  const [name, setname] = useState("User1");
+  const [profileview,setprofileviews] = useState(0);
+  const [impression,setimpression] = useState(123);
+  const [position,setpositions] = useState("SDE");
+//   let profileviews;
+//   let impressions;
+//   let position;
+
+  
+
+  
+//   profileviews = 0;
+//   impressions = 0;
+
+
+
+  const handleclick = () => {
+    setname("User-2");
+    setprofileviews(profileview+1);
+    setimpression(impression+23);
+    setpositions("Data Analyst");
+  };
+  return (
+    <>
+      <h1>{name} </h1>
+      <p>{position}</p>
+      <p>Who viewed your profile : {profileview} </p>
+      <p>Imprssions on your post : {impression} </p>
+      <button
+        onClick={() => {
+          handleclick();
+        }}
+        className="btn btn-primary"
+      >
+        Change Name
+      </button>
+    </>
+  );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './Components/Navbar'
-// import Center from './Components/Center'
+// import Navbar from './Components/Navbar'
+import Center from './Components/Center'
 // import List from './Components/List'
 // import Form from './Components/Form'
 // import Footer from './Components/Footer'
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
    <div>
-     <Navbar />
+     {/* <Navbar /> */}
     {/* <Center/>
     <List/>
+    
     <Form/>
     <Footer/> */}
+    {/* <List/> */}
+    <Center/>
    </div>
 
 );
